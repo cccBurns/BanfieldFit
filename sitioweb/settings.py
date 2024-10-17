1d014d17ad9eba68d04dc94d2fdf1a991207d178
christian burns <christianburns03@gmail.com>
2024-10-17 10:45:59 -0300

    juevesd

--- sitioweb/settings.py
+++ sitioweb/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Agregar middleware de Whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
